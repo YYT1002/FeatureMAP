48d2e465831a560f610600416fa1b27e4d522f35
yang_uqdi <yangyangnwpu@gmail.com>
2024-06-11 14:53:40 +1000

    update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("README.md", "r") as fh:
     
 configuration = {
     'name' : 'featuremap-learn',
-    'version': '0.2.2',
+    'version': '0.2.3',
     'description' : 'FeatureMAP',
     'long_description' : long_description,
     'long_description_content_type' : "text/markdown",
