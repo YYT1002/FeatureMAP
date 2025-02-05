6507b8d5386f0f9f48f76465ccc2990d85e8621f
Yang Yang <yangyangnwpu@gmail.com>
2025-02-06 09:47:16 +1000

    Delete __init__.py

--- __init__.py
+++
@@ -1,7 +0,0 @@
-from warnings import warn, catch_warnings, simplefilter
-from .featuremap_ import FeatureMAP
-
-# Workaround: https://github.com/numba/numba/issues/3341
-import numba
-
-import pkg_resources
